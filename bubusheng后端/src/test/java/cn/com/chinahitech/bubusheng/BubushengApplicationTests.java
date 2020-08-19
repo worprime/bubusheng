0ba467610aaa4c2b7a84faea10cbdb8aa2f97f88
worprime <37793599+worprime@users.noreply.github.com>
2020-08-19 22:06:27 +0800

    Add files via upload

---
+++ bubusheng后端/src/test/java/cn/com/chinahitech/bubusheng/BubushengApplicationTests.java
@@ -0,0 +1,13 @@
+package cn.com.chinahitech.bubusheng;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BubushengApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
